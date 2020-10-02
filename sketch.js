9f5f4282dc321f26dbfaa805a4448698b3a5d1d5
Mayank <70268410+Mayank300@users.noreply.github.com>
2020-10-02 21:26:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+var backIMG, database;
+var canvas;
+var gameState = 0;
+var playerCount;
+var form,player,game;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+}
+
+function draw(){
+  //background("white");
+  
+}
